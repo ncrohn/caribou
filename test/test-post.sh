1f170b4290a1d6da51a6906fc2f53abe477f7590
ncrohn <nick@meltmedia.com>
2012-03-07 18:01:13 -0700

    Changed path in test curl

--- test/test-post.sh
+++ test/test-post.sh
@@ -1,1 +1,1 @@
-curl --header 'x-github-event: push' --data-urlencode payload@$1 http://ec2-184-73-85-148.compute-1.amazonaws.com:4050/
+curl --header 'x-github-event: push' --data-urlencode payload@$1 http://localhost:4050/
